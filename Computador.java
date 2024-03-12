b986eaee9e02038c1a6c5e6bd68cb9d6f1da52b8
JoaoPauloDantas <109078725+joaopdantas001@users.noreply.github.com>
2024-03-12 15:05:51 -0300

    versão 1.0
    sistema funcionando como solicitado para avaliação. agora, quero implementar JOptionPane.

--- Computador.java
+++ Computador.java
@@ -1,135 +1,74 @@
-public class Computador { 
-
-  
-
-    private String marca; 
-
-    private String modelo; 
-
-    private int anoFabricacao; 
-
-    private String processador; 
-
-    private double memoriaRAM; 
-
-  
-
-    public Computador() { 
-
-    } 
-
-  
-
-    public Computador(String marca, String modelo, int anoFabricacao, String processador, double memoriaRAM) { 
-
-        this.marca = marca; 
-
-        this.modelo = modelo; 
-
-        this.anoFabricacao = anoFabricacao; 
-
-        this.processador = processador; 
-
-        this.memoriaRAM = memoriaRAM; 
-
-    } 
-
-  
-
-    public String getMarca() { 
-
-        return marca; 
-
-    } 
-
-  
-
-    public void setMarca(String marca) { 
-
-        this.marca = marca; 
-
-    } 
-
-  
-
-    public String getModelo() { 
-
-        return modelo; 
-
-    } 
-
-  
-
-    public void setModelo(String modelo) { 
-
-        this.modelo = modelo; 
-
-    } 
-
-  
-
-    public int getAnoFabricacao() { 
-
-        return anoFabricacao; 
-
-    } 
-
-  
-
-    public void setAnoFabricacao(int anoFabricacao) { 
-
-        this.anoFabricacao = anoFabricacao; 
-
-    } 
-
-  
-
-    public String getProcessador() { 
-
-        return processador; 
-
-    } 
-
-  
-
-    public void setProcessador(String processador) { 
-
-        this.processador = processador; 
-
-    } 
-
-  
-
-    public double getMemoriaRAM() { 
-
-        return memoriaRAM; 
-
-    } 
-
-  
-
-    public void setMemoriaRAM(double memoriaRAM) { 
-
-        this.memoriaRAM = memoriaRAM; 
-
-    } 
-
-  
-
-    public void imprimir() { 
-
-        System.out.println("Marca: " + marca); 
-
-        System.out.println("Modelo: " + modelo); 
-
-        System.out.println("Ano de Fabricação: " + anoFabricacao); 
-
-        System.out.println("Processador: " + processador); 
-
-        System.out.println("Memória RAM: " + memoriaRAM + " GB"); 
-
-        System.out.println(); 
-
-    } 
-
-} 
+public class Computador {
+
+    // Atributos
+    private String marca;
+    private String modelo;
+    private int anoFabricacao;
+    private String processador;
+    private int memoriaRAM;
+
+    // Construtor Default
+    public Computador() {
+    }
+
+    // Construtor com parâmetros
+    public Computador(String marca, String modelo, int anoFabricacao, String processador, int memoriaRAM) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoFabricacao = anoFabricacao;
+        this.processador = processador;
+        this.memoriaRAM = memoriaRAM;
+    }
+
+    // Setters e Getters
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setProcessador(String processador) {
+        this.processador = processador;
+    }
+
+    public String getProcessador() {
+        return processador;
+    }
+
+    public void setMemoriaRAM(int memoriaRAM) {
+        this.memoriaRAM = memoriaRAM;
+    }
+
+    public double getMemoriaRAM() {
+        return memoriaRAM;
+    }
+
+    // Método para imprimir os atributos
+    public void imprimir() {
+        System.out.println("______________________");
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano de Fabricação: " + anoFabricacao);
+        System.out.println("Processador: " + processador);
+        System.out.println("Memória RAM: " + memoriaRAM + "GB");
+        System.out.println();
+    }
+}
